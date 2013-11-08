ac4e64a84f9099f43dc13cf08453575dd7ad1630
Desman <artem.kirienko@gmail.com>
2013-11-08 22:46:14 +0400

    Shell scripts to execute on connection/disconnection

---
+++ nomonitor.sh
@@ -0,0 +1,12 @@
+osascript -e 'tell application "System Preferences"
+    reveal anchor "mouseTab" of pane id "com.apple.preference.mouse"
+end tell
+tell application "System Events" to tell process "System Preferences"
+    click checkbox 1 of window 1
+end tell
+quit application "System Preferences"
+tell application "System Events"
+	tell dock preferences
+		set properties to {screen edge:right}
+	end tell
+end tell'
